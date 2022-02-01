ce7c14b8d211fdb05a78d0e6a88ad14839a46d55
Andrey Kudinov <2499931@gmail.com>
2022-02-01 18:16:21 +0400

    add prettier and eslint 📝

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended' // Make sure this is always the last element in the array.
+  ],
+  plugins: ['simple-import-sort', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    semi: ['error', 'never'],
+    'comma-dangle': ['error', 'never']
+  }
+}
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'none',
+  singleQuote: true,
+  printWidth: 90,
+  tabWidth: 2,
+  endOfLine: 'auto',
+  semi: false,
+}
