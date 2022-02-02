a61c1e784b40297b74866e8627f1a866a7cf129b
Andrey Kudinov <2499931@gmail.com>
2022-02-02 21:00:00 +0400

    add author

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './style.scss'
+import './lib/core'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
@@ -6,3 +7,4 @@ app.innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+console.log(document.querySelectorAll('h1'))
